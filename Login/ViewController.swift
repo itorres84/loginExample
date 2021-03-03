4f769f0ad968e7347ce754f6d1b310a4fd698167
Israel Torres <israeltorres27@gmail.com>
2021-03-02 20:12:03 -0600

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Israel Torres Alvarado on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
